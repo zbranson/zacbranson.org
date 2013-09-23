70aa2968ab5bc6147e9171cae8090a89b9238746
Lwin Moe <lwinmoe@gmail.com>
2013-09-22 22:30:56 -0400

    robots.txt added.

--- src/sites/my_site/my_site/urls.py
+++ src/sites/my_site/my_site/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import *
 from django.conf import settings
+from django.http import HttpResponse
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -20,3 +21,7 @@ urlpatterns = patterns('',
 if not settings.PRODUCTION:
     urlpatterns += patterns('',
         (r'^assets/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes':False}),)
+
+urlpatterns += patterns('',
+    (r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: ", mimetype="text/plain")),
+)
