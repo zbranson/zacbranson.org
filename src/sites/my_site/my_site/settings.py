7c7d6bb62ee76d4d622c180bd91249a775411ef7
Lwin Moe <lwinmoe@gmail.com>
2013-09-25 17:52:48 -0400

    404.html and 500.html. settings.py

--- src/sites/my_site/my_site/settings.py
+++ src/sites/my_site/my_site/settings.py
@@ -3,7 +3,7 @@ import os
 import sys
 import socket
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ugettext = lambda s: s
@@ -21,9 +21,6 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
 LOCALE_PATHS = (os.path.join(PROJECT_ROOT, 'locale'),)
 
 PRODUCTION  = True
-DEBUG = True
-
-TEMPLATE_DEBUG = DEBUG
 
 import secret
 DATABASES = {
@@ -39,7 +36,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".lwinmoe.org"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
